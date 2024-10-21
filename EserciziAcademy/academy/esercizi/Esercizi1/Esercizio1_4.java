f02397c1f01b74410df646ce95a125352403ed4b
ITMASTER\G.Zaffinelli-cons <G.Zaffinelli-cons@reactive-almaviva.it>
2024-10-21 16:43:04 +0200

    Esercizio1.4

---
+++ EserciziAcademy/academy/esercizi/Esercizi1/Esercizio1_4.java
@@ -0,0 +1,21 @@
+package academy.esercizi.Esercizi1;
+
+/* Scrivere un programma che definisce un array di 10 elementi e popolarlo con valori di fantasia,
+si cancelli l'elemento a posizione 4 facendo shiftare i valori di tutti gli elementi successivi
+a sinistra di uno */
+public class Esercizio1_4 {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] newNum = new int[8];
+
+        for(int i = 3; i<num.length-1; i++){
+            num[i] = num[i+1];
+
+        }
+        num[num.length-1] = 0;
+
+        for (int j : num) {
+            System.out.println(j);
+        }
+    }
+}
