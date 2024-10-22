401d117b00c219143fa5203eacc58e1e476aa7d2
ITMASTER\G.Zaffinelli-cons <G.Zaffinelli-cons@reactive-almaviva.it>
2024-10-22 16:18:44 +0200

    Esercizio 7 modulo 5

---
+++ EserciziAcademy/academy/esercizi/esercizi5/Esercizio7.java
@@ -0,0 +1,23 @@
+package academy.esercizi.esercizi5;
+
+/*Vogliamo rimuovere tutti gli spazi e i trattini presenti nella stringa creditCardNumber:
+ad esempio, se la stringa fosse "4123-5678-9012-3450”, dovremmo trasformarla
+in "4123567890123450". Non usare i metodi replace e replaceAll sulla classe String ma ciclare
+i singoli caratteri
+ */
+public class Esercizio7 {
+    public static void main(String[] args) {
+        System.out.println(rimuoviSpazi("4434355455-554  55545"));
+    }
+
+    public static String rimuoviSpazi(String creditCardNumber) {
+        String result = "";
+        for (int i = 0; i < creditCardNumber.length() - 1; i++) {
+            if (creditCardNumber.charAt(i) != ' ' && creditCardNumber.charAt(i)!= '-') {
+                result += creditCardNumber.charAt(i);
+            }
+        }
+
+        return result;
+    }
+}
