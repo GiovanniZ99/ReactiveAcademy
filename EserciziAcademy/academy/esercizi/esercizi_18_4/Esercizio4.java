8974774431132ab56c9545727ee0a79d24be994a
ITMASTER\G.Zaffinelli-cons <G.Zaffinelli-cons@reactive-almaviva.it>
2024-10-30 12:03:19 +0100

    Esercizio 18.4 completato

---
+++ EserciziAcademy/academy/esercizi/esercizi_18_4/Esercizio4.java
@@ -0,0 +1,83 @@
+package academy.esercizi.esercizi_18_4;
+
+import java.util.Scanner;
+/* Scrivete un programma che simuli una transazione bancaria. Occorre gestire due conti
+bancari: un conto corrente (checking account) e un conto di risparmio (savings account).
+Per prima cosa chiedete all’utente i saldi iniziali dei due conti, rifiutando saldi negativi.
+Poi chiedete quale sia il tipo di operazione da eseguire: versamento (deposit), prelievo
+(withdrawal) o bonifico (transfer). Ancora, chiedete il tipo di conto: checking o savings.
+Rifiutate transazioni che rendano negativo il saldo di un conto. Infine, visualizzate i saldi
+dei due conti dopo l’operazione richiesta.
+ */
+
+public class Esercizio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci il saldo del conto corrente");
+        double saldoCheck = scanner.nextDouble();
+        System.out.println("Inserisci il saldo del conto di risparmio");
+        double saldoSav = scanner.nextDouble();
+        if (saldoCheck < 0 || saldoSav < 0) {
+            System.out.println("Importi non validi");
+        }
+        System.out.println("Inserire l'operazione che si desidera effetuare");
+        System.out.println("Premere uno per un prelievo sul conto corrente");
+        System.out.println("Premere due per un prelievo sul conto risparmio");
+        System.out.println("Premere tre per un deposito sul conto corrente");
+        System.out.println("Premere quattro per un deposito sul conto risparmio");
+        System.out.println("Premere cinque per trasferire una cifra da un conto a un altro");
+        int scelta = scanner.nextInt();
+        double cifra = scanner.nextDouble();
+        switch (scelta) {
+            case 1:
+                withdrawal(saldoCheck, cifra);
+                break;
+            case 2:
+                withdrawal(saldoSav, cifra);
+                break;
+            case 3:
+                deposit(saldoCheck, cifra);
+                break;
+            case 4:
+                deposit(saldoSav, cifra);
+                break;
+            case 5:
+                transfer(saldoCheck, saldoSav, cifra);
+                break;
+            case 6:
+                transfer(saldoSav, saldoCheck, cifra);
+                break;
+            default:
+                System.out.println("Numero inserito non valido");
+        }
+    }
+
+    public static void withdrawal(double conto, double prelievo) {
+        if (prelievo <= conto) {
+            conto -= prelievo;
+            System.out.println(conto);
+        } else {
+            System.out.println("Importo non valido");
+        }
+    }
+
+    public static void deposit(double conto, double prelievo) {
+        if (prelievo > 0) {
+            conto += prelievo;
+            System.out.println(conto);
+        } else {
+            System.out.println("Importo non valido");
+        }
+    }
+
+    public static void transfer(double contoMittente, double contoDestinatario, double cifraDaTrasferire) {
+        if (cifraDaTrasferire <= contoMittente) {
+            contoDestinatario += cifraDaTrasferire;
+            contoMittente -= cifraDaTrasferire;
+            System.out.println(contoDestinatario);
+            System.out.println(contoMittente);
+        } else {
+            System.out.println("Importo non valido");
+        }
+    }
+}
