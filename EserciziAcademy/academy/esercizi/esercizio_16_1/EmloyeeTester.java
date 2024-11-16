d10ac8b6f990377053c35c86e8d8f5a225bf7848
ITMASTER\G.Zaffinelli-cons <G.Zaffinelli-cons@reactive-almaviva.it>
2024-11-16 09:41:00 +0100

    Correzione 16.1

--- EserciziAcademy/academy/esercizi/esercizio_16_1/EmloyeeTester.java
+++ EserciziAcademy/academy/esercizi/esercizio_16_1/EmloyeeTester.java
@@ -1,8 +1,17 @@
 package academy.esercizi.esercizio_16_1;
-
+/*Realizzate la classe Employee (dipendente). Ogni dipendente ha un nome (una stringa)
+e uno stipendio (di tipo double). Scrivete un costruttore con due parametri:public
+Employee(String employeeName, double currentSalary) e i metodi:public String getName()
+public double getSalary() public void raiseSalary(double byPercent). Tali metodi forniscono,
+nell’ordine, il nome e lo stipendio del dipendente e ne aumentano il salario della percentuale
+ indicata. Ecco un esempio di utilizzo:Employee harry = new Employee("Hacker, Harry", 50000);
+ harry.raiseSaIary(io); // Harry ottiene un aumento di stipendio del 10%. Progettate anche la
+ classe EmployeeTester che collaudi tutti i metodi.
+ */
 public class EmloyeeTester {
     public static void main(String[] args) {
         Employee harry = new Employee("Harry", 5000);
         harry.raiseSalary(100);
+        System.out.println(harry.getCurrentSalary());
     }
 }
