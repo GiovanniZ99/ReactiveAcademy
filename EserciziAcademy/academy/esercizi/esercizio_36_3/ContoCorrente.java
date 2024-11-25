c47663dc71e4179505bc48f2ea747803626f91a7
ITMASTER\G.Zaffinelli-cons <G.Zaffinelli-cons@reactive-almaviva.it>
2024-11-25 15:02:57 +0100

    Correzione 36.3

--- EserciziAcademy/academy/esercizi/esercizio_36_3/ContoCorrente.java
+++ EserciziAcademy/academy/esercizi/esercizio_36_3/ContoCorrente.java
@@ -2,7 +2,7 @@ package academy.esercizi.esercizio_36_3;
 
 public class ContoCorrente {
     private double saldo;
-    private String numeroConto;
+    private final String numeroConto;
 
     public ContoCorrente(double saldo, String numeroConto) {
         this.saldo = saldo;
@@ -24,20 +24,4 @@ public class ContoCorrente {
         throw new ErroreVersamento("Importo superiore al saldo", importo, this.numeroConto, this.saldo, importo);
     }
 
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    public String getNumeroConto() {
-        return numeroConto;
-    }
-
-    public void setNumeroConto(String numeroConto) {
-        this.numeroConto = numeroConto;
-    }
 }
