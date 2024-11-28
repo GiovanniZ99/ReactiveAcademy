cf7872c74a1613b1155986fdff087494b84e5680
ITMASTER\G.Zaffinelli-cons <G.Zaffinelli-cons@reactive-almaviva.it>
2024-11-28 20:15:36 +0100

    Correzione 37.8

--- EserciziAcademy/academy/esercizi/esercizio_37_8/Test.java
+++ EserciziAcademy/academy/esercizi/esercizio_37_8/Test.java
@@ -1,29 +1,24 @@
 package academy.esercizi.esercizio_37_8;
 
-import academy.esercizi.esercizi0_30_1.FillinQuestion;
-import academy.esercizi.esercizi0_30_1.FreeResponse;
-import academy.esercizi.esercizi0_30_1.NumericQuestion;
-import academy.esercizi.esercizi0_30_1.Question;
-
-/**/
+/*Modificare il progetto per gestire i quiz ed implementare le classi con l’uso dei generics per
+gestire le differenze tra risposta salvata in un numero, in una lista o come String.
+Per le specifiche complete è possibile guardare anche il file «EsercizioQuestionari.docx»*/
 public class Test {
     public static void main(String[] args) {
-        academy.esercizi.esercizi0_30_1.Question q = new FillinQuestion();
+        Question <String> q = new FillinQuestion();
         q.setAnswer("La capitale dell’Italia è Roma, si trova nella regione Lazio ed ha 2800000 abitanti");
         q.setText("La capitale dell’Italia è ****, si trova nella regione **** ed ha **** abitanti");
         q.display();
         System.out.println(q.checkAnswer("Roma,Lazio,280000"));
 
-        academy.esercizi.esercizi0_30_1.Question q2 = new NumericQuestion();
-        q2.setAnswer("25");
-        q2.setText("Quanto fa 5 + 20?");
+     Question <Integer> q2 = new NumericQuestion();
+        q2.setAnswer(25);
+        q2.setText(Integer.valueOf("Quanto fa 5 + 20?"));
         q2.display();
 
-        System.out.println(q2.checkAnswer("25"));
-        System.out.println(q2.checkAnswer("abc"));
+        System.out.println(q2.checkAnswer(25));
 
-
-        Question q3 = new FreeResponse();
+        Question <String>q3 = new FreeResponse();
         q3.setAnswer("Roma");
         q3.setText("Qual è la capitale dell'Italia?");
 
