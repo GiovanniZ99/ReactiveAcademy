2bf6e1cab8e4b9534280672eb592839dc31749f7
ITMASTER\G.Zaffinelli-cons <G.Zaffinelli-cons@reactive-almaviva.it>
2025-02-04 17:37:52 +0100

    esercizi python

---
+++ EserciziAcademy/academy/eserciziPython/main.py
@@ -0,0 +1,22 @@
+def main():
+
+    print("r")
+
+#commento
+
+x = 5
+y = "john"
+
+print(x)
+
+dictionario = {"book" : 1}
+
+print(dictionario)
+
+fruit = ["apple", "banana", "cherry"]
+
+for x in fruit:
+    print(x)
+
+if __name__ == "__main__":
+    main()
