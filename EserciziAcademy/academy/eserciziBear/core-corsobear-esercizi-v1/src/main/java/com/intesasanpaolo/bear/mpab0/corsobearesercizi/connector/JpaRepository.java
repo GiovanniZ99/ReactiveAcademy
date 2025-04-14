82540e0fb262d622eb37d0f7c970b70cf1134208
Giovanni Zaffinelli <160517190+GiovanniZ99@users.noreply.github.com>
2025-04-14 15:20:34 +0200

    Delete dir

--- EserciziAcademy/academy/eserciziBear/core-corsobear-esercizi-v1/src/main/java/com/intesasanpaolo/bear/mpab0/corsobearesercizi/connector/JpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.intesasanpaolo.bear.mpab0.corsobearesercizi.connector;
-import com.intesasanpaolo.bear.connector.jpa.JPAConnector;
-
-import com.intesasanpaolo.bear.mpab0.corsobearesercizi.model.CountryModel;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaRepository  extends JPAConnector<CountryModel, Long>{
-}
--- EserciziAcademy/academy/eserciziBear/core-corsobear-esercizi-v1/src/main/java/com/intesasanpaolo/bear/mpab0/corsobearesercizi/exception/CountryNonTrovatoException.java
+++
@@ -1,7 +0,0 @@
-package com.intesasanpaolo.bear.mpab0.corsobearesercizi.exception;
-
-public class CountryNonTrovatoException extends RuntimeException{
-    public CountryNonTrovatoException(){
-        super("Country non trovato");
-    }
-}
